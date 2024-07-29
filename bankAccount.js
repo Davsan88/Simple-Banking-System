29237c552a1bf44313cf7ca519b14e417cae46cb
DavSan <david.sanchez.martinez@outlook.com>
2024-07-29 19:37:38 +0100

    Update bankAccount.js

--- bankAccount.js
+++ bankAccount.js
@@ -6,8 +6,9 @@ class BankAccount {
         this.balance = balance;
     }
 
+    // Implement methods for depositing, withdrawing, and checking balance
     deposit(amount) {
-        
+        this.balance += amount; // Access the current balance and add the deposit amount to it
     }
 
     withdraw(amount) {
