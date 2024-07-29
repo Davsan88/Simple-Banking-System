4a5a19b3a64459123110238b289d93f9594885d5
DavSan <david.sanchez.martinez@outlook.com>
2024-07-29 19:28:48 +0100

    Update bankAccount.js

--- bankAccount.js
+++ bankAccount.js
@@ -1,7 +1,20 @@
+// Define class called BankAccount and its properties
 class BankAccount {
     constructor(accountNumber, accountHolder, balance){
         this.accountNumber = accountNumber;
         this.accountHolder = accountHolder;
         this.balance = balance;
+    }
+
+    deposit(amount) {
+        
+    }
+
+    withdraw(amount) {
+
+    }
+
+    checkBalance() {
+
     }
 };
