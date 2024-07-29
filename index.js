b63aa79660b159a0723f221109c9221b1b555799
DavSan <david.sanchez.martinez@outlook.com>
2024-07-29 20:34:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-import { BankAccount } from "./bankAccount";
+import { BankAccount } from "./bankAccount.js";
 
+
+// Create instances of BankAccount
+const account1 = new BankAccount("1234", "D. Sanchino", 50);
+const account2 = new BankAccount("5678", "S. Sanchulla", 200);
+
+// Demonstrate the usage of methods
+console.log(account1.checkBalance()); // Should show initial balance
+account1.deposit(200);
+console.log(account1.checkBalance()); // Should show updated balance after deposit
+account1.withdraw(50);
+console.log(account1.checkBalance()); // Should show updated balance after withdrawal
+
+console.log(account2.checkBalance()); // Should show initial balance
+account2.withdraw(100);
+console.log(account2.checkBalance()); // Should show updated balance after withdrawal
+account2.deposit(300);
+console.log(account2.checkBalance()); // Should show updated balance after deposit
